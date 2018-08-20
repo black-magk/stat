5494ce6246af12a8993ae7c9da39e00a280a6051
Landon Carr <landoncarr@landons-mbp.attlocal.net>
2018-08-20 15:22:10 -0700

    Pods Installed

---
+++ stat/stat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  stat
+//
+//  Created by Landon Carr on 8/20/18.
+//  Copyright © 2018 Landon Carr. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ stat/stat/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  stat
+//
+//  Created by Landon Carr on 8/20/18.
+//  Copyright © 2018 Landon Carr. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
